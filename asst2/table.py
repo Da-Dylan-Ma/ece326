822c99d6b0553b8159e4d0510ecc3593b987f00f
pyuxiang <pyxelise@gmail.com>
2019-09-25 21:50:22 -0400

    Complete table.py for assignment 2

--- asst2/table.py
+++ asst2/table.py
@@ -4,8 +4,13 @@
 #
 # Implements a two-dimension table where all cells must be of same type
 #
+# Note: completed
+#
 
+from collections import defaultdict
 from collections.abc import Sized
+# Sized requires subclasses to implement __len__()
+
 
 class Table:
     #
@@ -23,10 +28,9 @@ class Table:
         self.xlabels = tuple(xlabels)
         self.ylabels = tuple(ylabels)
         self.unit = unit
+        self.data = defaultdict(lambda: defaultdict(lambda: None))
+        # using defaultdict since no extra code for initialization
 
-        # TODO: finish me
-        return
-    
     #
     # "private" member function to validate key
     #
@@ -36,49 +40,40 @@ class Table:
         if len(key) != 2:
             raise KeyError("key must have exactly two elements")
         # unpack key to row and column
-        row, col = key    
+        row, col = key
         if row not in self.ylabels:
             raise KeyError("%s is not a valid y-label"%str(row))
         if col not in self.xlabels:
             raise KeyError("%s is not a valid x-label"%str(col))
         return row, col
-        
+
     #
     # Overloads index operator for assigning to a cell
     #
     # key: key of the cell
     # value: value of the cell (must be of type 'celltype')
-    #    
+    #
     def __setitem__(self, key, value):
         if not isinstance(value, self.celltype):
             raise TypeError("value must be of type %s"%(self.celltype.__name__))
         row, col = self._validate_key(key)
-        
-        # TODO: implement me
-        return
+        self.data[row][col] = value
 
     #
     # Overloads index operator for retrieving a value from a cell
     #
     # key: key of the cell
-    #    
+    #
     def __getitem__(self, key):
-        row, col = self._validate_key(key) 
-        
-        # TODO: implement me
-        return None
+        row, col = self._validate_key(key)
+        return self.data[row][col]
 
     #
     # Overloads index operator for deleting a cell's value. You should
     # set the cell's value back to None
     #
     # key: key of the cell
-    #  
+    #
     def __delitem__(self, key):
-        row, col = self._validate_key(key) 
-        
-        # TODO: implement me
-        return
-    
-            
-
+        row, col = self._validate_key(key)
+        self.data[row][col] = None
