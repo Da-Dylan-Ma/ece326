9a82bf3a5c1dea3a040e9386defad604012ee743
pyuxiang <pyxelise@gmail.com>
2019-09-26 16:32:08 -0400

    Update Hand.code() to account for dealer hit/stand

--- asst2/easybj.py
+++ asst2/easybj.py
@@ -8,54 +8,29 @@
 from table import Table
 from collections import defaultdict
 
-# code names for all the hard hands
-HARD_CODE = [ '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14',
-    '15', '16', '17', '18', '19', '20']
+HARD_CODE =['4','5','6','7','8','9','10','11','12','13','14',
+            '15','16','17','18','19','20'] # hard hand codes
+SOFT_CODE = ['AA','A2','A3','A4','A5','A6','A7','A8','A9'] # soft hand codes
+SPLIT_CODE = ['22','33','44','55','66','77','88','99','TT','AA'] # can split
+NON_SPLIT_CODE = HARD_CODE + SOFT_CODE # cannot split
+STAND_CODE = HARD_CODE + ['21'] + SOFT_CODE # can stand
+PLAYER_CODE = HARD_CODE + SPLIT_CODE + SOFT_CODE[1:] # strategy table y-labels
+DEALER_CODE = HARD_CODE + SOFT_CODE[:6] # strategy table x-labels
 
-# code names for all the soft hands
-SOFT_CODE = [ 'AA', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9' ]
-
-# code names for all the hands that can be split
-SPLIT_CODE = [ '22', '33', '44', '55', '66', '77', '88', '99', 'TT', 'AA' ]
-
-# code names for all the hands that cannot be split
-NON_SPLIT_CODE = HARD_CODE + SOFT_CODE
-
-# code names for standing
-STAND_CODE = HARD_CODE + ['21'] + SOFT_CODE
-
-# code names for all the y-labels in the strategy table
-PLAYER_CODE = HARD_CODE + SPLIT_CODE + SOFT_CODE[1:]
-
-# code names for all the x-labels in all the tables
-DEALER_CODE = HARD_CODE + SOFT_CODE[:6]
-
-# code names for all the initial player starting hands
-# (hard 4 is always 22, and hard 20 is always TT)
+# All possible starting hands (hard 4 is always 22, and hard 20 is always TT)
 INITIAL_CODE = HARD_CODE[1:-1] + SPLIT_CODE + SOFT_CODE[1:] + ['BJ']
 
-#
-# Returns whether a and b are close enough in floating point value
-# Note: use this to debug your code
-#
+BUST = 0 # representation of busted hand
+DISTINCT = ['A','2','3','4','5','6','7','8','9','T'] # distinct card values
+NUM_FACES = 4 # number of cards with 10 points
+NUM_RANKS = 13 # number of ranks in a French deck
+
 def isclose(a, b=1., rel_tol=1e-09, abs_tol=0.0):
+    """ Returns whether a and b are close enough in floating point value """
     return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
-# use the numeral value 0 to represent a busted hand (makes it easier to
-# compare using integer comparison)
-BUST = 0
-
-# list of distinct card values
-DISTINCT = [ 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T' ]
-
-# number of cards with 10 points
-NUM_FACES = 4
-
-# number of ranks in a French deck
-NUM_RANKS = 13
-
-# return the probability of receiving this card
 def probability(card):
+    """ Returns the probability of receiving this card """
     return (1 if card != 'T' else NUM_FACES) / NUM_RANKS
 
 def card_value(card):
@@ -64,45 +39,42 @@ def card_value(card):
     if card == "T": return 10
     return int(card)
 
-#
-# Represents a Blackjack hand (owned by either player or dealer)
-#
-# Note: you should make BIG changes to this class
-#
 class Hand:
+    """ Represents a Blackjack hand (owned by either player or dealer) """
     def __init__(self, x, y, dealer=False):
         self.cards = [x, y]
         self.is_dealer = dealer
 
-    # probability of receiving this hand
     def probability(self):
+        """ Returns the probability of receiving the hand as a float """
         p = 1.
         for c in self.cards:
             p *= probability(c)
         return p
 
-    # the code which represents this hand
-    # assuming nosplit means the hand cannot split
     def code(self, nosplit=False):
-        # assuming self.cards may have more than 2 cards, bust == hard value
+        """ Returns the 'XX' code that represents the hand, 0 if busted
+            nosplit: True if hand can split """
+        # Note: self.cards may have more than 2 cards
         value = sum(map(card_value, self.cards))
-
         if len(self.cards) == 2:
             if self.cards[0] == self.cards[1]:
-                if not nosplit or self.cards[0] == "A":
-                    return self.cards[0]*2 # split hands
-            if value == 11: return "BJ" # blackjack
+                if self.cards[0] == "A": return "AA" # special AA
+                if not nosplit and not self.is_dealer:
+                    return self.cards[0]*2 # split
+            if value == 11 and "A" in self.cards: return "BJ" # blackjack
 
+        if value > 21: return BUST # bust
         if value >= 11: return str(value) # hard
-        if "A" in self.cards: return "A" + str(value-1) # soft
+        if "A" in self.cards:
+            if not self.is_dealer or value <= 7:
+                return "A" + str(value-1) # soft
+            return str(value+10) # dealer cannot hit > A6
         return str(value) # hard
 
-#
-# Singleton class to store all the results.
-#
-# Note: you should make HUGE changes to this class
-#
 class Calculator:
+    """ Singleton class to store all the results. """
+
     def __init__(self):
         self.initprob = Table(float, DEALER_CODE + ['BJ'], INITIAL_CODE, unit='%')
         self.dealprob = defaultdict(dict)
@@ -114,8 +86,8 @@ class Calculator:
         self.strategy = Table(str, DEALER_CODE, PLAYER_CODE)
         self.advantage = 0.
 
-    # make each cell of the initial probability table
     def make_initial_cell(self, player, dealer):
+        """ Populate initial probability table """
         table = self.initprob
         dc = dealer.code()
         pc = player.code()
@@ -125,8 +97,8 @@ class Calculator:
         else:
             table[pc,dc] += prob
 
-    # make the initial probability table
     def make_initial_table(self):
+        """ Initialize probability table """
         #
         # TODO: refactor so that other table building functions can use it
         #
@@ -138,21 +110,18 @@ class Calculator:
                         player = Hand(x, y)
                         self.make_initial_cell(player, dealer)
 
-    # verify sum of initial table is close to 1
     def verify_initial_table(self):
+        """ Verify sum of initial table is close to 1 """
         total = 0.
         for x in self.initprob.xlabels:
             for y in self.initprob.ylabels:
                 total += self.initprob[y,x]
         assert(isclose(total))
 
-#
-# Calculate all the ev tables and the final strategy table and return them
-# all in a dictionary
-#
 def calculate():
+    """ Returns a dictionary containing all calculated ev tables and
+        final strategy table """
     calc = Calculator()
-
     calc.make_initial_table()
 
     # TODO: uncomment once you finished your table implementation
@@ -172,3 +141,6 @@ def calculate():
         'strategy' : calc.strategy,
         'advantage' : calc.advantage,
     }
+
+if __name__ == "__main__":
+    print(calculate())
--- asst2/test_easybj.py
+++ asst2/test_easybj.py
@@ -1,13 +1,14 @@
 import unittest
-from easybj import Hand
+from easybj import *
 
 class TestHandMethods(unittest.TestCase):
 
     def setUp(self):
+        ### PLAYER ###
         # 21-value hands
         self.h_at = Hand("A", "T")
         self.h_ta = Hand("T", "A")
-        self.h_tta = Hand("T", "T", "A")
+        self.h_tta = Hand("T", "T")
         self.h_tta.cards.append("A")
 
         # split hands
@@ -23,8 +24,12 @@ class TestHandMethods(unittest.TestCase):
         self.h_t9 = Hand("T", "9")
         self.h_a84 = Hand("A", "8")
         self.h_a84.cards.append("4")
+        self.h_65 = Hand("6", "5")
+        self.h_89 = Hand("8", "9")
 
         # soft hands
+        self.h_a6 = Hand("A", "6")
+        self.h_a7 = Hand("A", "7")
         self.h_a8 = Hand("A", "8")
         self.h_8a = Hand("8", "A")
         self.h_44a = Hand("4", "4")
@@ -38,8 +43,49 @@ class TestHandMethods(unittest.TestCase):
         self.h_t68 = Hand("T", "6")
         self.h_t68.cards.append("8")
 
+        ### DEALER ###
+        # 21-value hands
+        self.d_at = Hand("A", "T", dealer=True)
+        self.d_ta = Hand("T", "A", dealer=True)
+        self.d_tta = Hand("T", "T", dealer=True)
+        self.d_tta.cards.append("A")
+
+        # split hands
+        self.d_aa = Hand("A", "A", dealer=True)
+        self.d_22 = Hand("2", "2", dealer=True)
+        self.d_55 = Hand("5", "5", dealer=True)
+        self.d_tt = Hand("T", "T", dealer=True)
+
+        # hard hands
+        self.d_23 = Hand("2", "3", dealer=True)
+        self.d_236 = Hand("2", "3", dealer=True)
+        self.d_236.cards.append("6")
+        self.d_t9 = Hand("T", "9", dealer=True)
+        self.d_a84 = Hand("A", "8", dealer=True)
+        self.d_a84.cards.append("4")
+        self.d_65 = Hand("6", "5", dealer=True)
+        self.d_a7 = Hand("A", "7", dealer=True)
+        self.d_a8 = Hand("A", "8", dealer=True)
+        self.d_8a = Hand("8", "A", dealer=True)
+        self.d_89 = Hand("8", "9", dealer=True)
+        self.d_44a = Hand("4", "4", dealer=True)
+        self.d_44a.cards.append("A")
+
+        # soft hands
+        self.d_a6 = Hand("A", "6", dealer=True)
+        self.d_aaaa = Hand("A", "A", dealer=True)
+        self.d_aaaa.cards.extend(["A", "A"])
+
+        # busted hands
+        self.d_t4a7 = Hand("T", "4", dealer=True)
+        self.d_t4a7.cards.extend(["A", "7"])
+        self.d_t68 = Hand("T", "6", dealer=True)
+        self.d_t68.cards.append("8")
+
     def test_code(self):
         _ = self.assertEqual
+
+        # player
         _(self.h_at.code(), "BJ")
         _(self.h_ta.code(), "BJ")
         _(self.h_tta.code(), "21")
@@ -56,12 +102,44 @@ class TestHandMethods(unittest.TestCase):
         _(self.h_236.code(), "11")
         _(self.h_t9.code(), "19")
         _(self.h_a84.code(), "13")
+        _(self.h_65.code(), "11")
+        _(self.h_89.code(), "17")
+        _(self.h_a6.code(), "A6")
+        _(self.h_a7.code(), "A7")
         _(self.h_a8.code(), "A8")
         _(self.h_8a.code(), "A8")
         _(self.h_44a.code(), "A8")
         _(self.h_aaaa.code(), "A3")
-        _(self.h_t4a7.code(), "22")
-        _(self.h_t68.code(), "24")
+        _(self.h_t4a7.code(), BUST)
+        _(self.h_t68.code(), BUST)
+
+        # dealer
+        _(self.d_at.code(), "BJ")
+        _(self.d_ta.code(), "BJ")
+        _(self.d_tta.code(), "21")
+        _(self.d_aa.code(), "AA")
+        _(self.d_22.code(), "4")
+        _(self.d_55.code(), "10")
+        _(self.d_tt.code(), "20")
+        _(self.d_aa.code(nosplit=True), "AA")
+        _(self.d_22.code(nosplit=True), "4")
+        _(self.d_55.code(nosplit=True), "10")
+        _(self.d_tt.code(nosplit=True), "20")
+        _(self.d_23.code(nosplit=True), "5")
+        _(self.d_23.code(), "5")
+        _(self.d_236.code(), "11")
+        _(self.d_t9.code(), "19")
+        _(self.d_a84.code(), "13")
+        _(self.d_65.code(), "11")
+        _(self.d_a6.code(), "A6")
+        _(self.d_a7.code(), "18")
+        _(self.d_a8.code(), "19")
+        _(self.d_8a.code(), "19")
+        _(self.d_89.code(), "17")
+        _(self.d_44a.code(), "19")
+        _(self.d_aaaa.code(), "A3")
+        _(self.d_t4a7.code(), BUST)
+        _(self.d_t68.code(), BUST)
 
 
 if __name__ == "__main__":
